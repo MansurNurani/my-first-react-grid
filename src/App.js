60d203cc2f36f216820fdc2a77e03e286dfa8a25
MansurNurani <mansur.nurani@gmail.com>
2021-10-02 12:12:24 +0330

    two grids width different header font-sizes

--- src/App.js
+++ src/App.js
@@ -2,29 +2,33 @@ import "./index.css"
 import React, { Component } from 'react'
 import "ag-grid-community/dist/styles/ag-grid.css"
 import "ag-grid-community/dist/styles/ag-theme-balham.css"
-import {AgGridReact} from 'ag-grid-react'
+import { AgGridReact } from 'ag-grid-react'
 export default class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      columnDefs:[
-        {headerName :'سازنده',field:'make',sortable:true,filter:true},
-        {headerName :'مدل',field:'model',sortable:true,filter:true},
-        {headerName :'قیمت',field:'price',sortable:true,filter:true},
-    ],
-    rowData:[
-      {make:'Toyota1',model:'model1',price:3000},
-      {make:'Toyota2',model:'model2',price:4000},
-      {make:'Toyota3',model:'model3',price:5000},
-    ]
+    this.state = {
+      columnDefs: [
+        { headerName: 'سازنده', field: 'make', sortable: true, filter: true },
+        { headerName: 'مدل', field: 'model', sortable: true, filter: true },
+        { headerName: 'قیمت', field: 'price', sortable: true, filter: true },
+      ],
+      rowData: [
+        { make: 'Toyota1', model: 'model1', price: 3000 },
+        { make: 'Toyota2', model: 'model2', price: 4000 },
+        { make: 'Toyota3', model: 'model3', price: 5000 },
+      ]
     }
   }
   render() {
     return (
-      <div className="ag-theme-balham" style={{margin:"0 auto", direction:"rtl", width:700,height:600}}>
-        <br />
-        <AgGridReact headerFont="Tahoma" headerClass="headerClass" enableRtl="true" columnDefs={this.state.columnDefs} rowData={this.state.rowData} />
-
+      <div style={{width:"fit-content", margin:"0px auto"}}>
+          <br />
+        <div className="div1 ag-theme-balham" style={{display:"inline-block", direction: "rtl", width: 620, height: 200 }}>
+          <AgGridReact enableRtl="true" columnDefs={this.state.columnDefs} rowData={this.state.rowData} />
+        </div>
+        <div className="div2 ag-theme-balham" style={{display:"inline-block", direction: "rtl", width: 620, height: 200 }}>
+          <AgGridReact enableRtl="true" columnDefs={this.state.columnDefs} rowData={this.state.rowData} />
+        </div>
       </div>
     )
   }
