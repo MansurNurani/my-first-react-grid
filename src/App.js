c23a24bb36955c508e008bc1362e1de73a8fa2d6
MansurNurani <mansur.nurani@gmail.com>
2021-10-02 10:08:24 +0330

    build folder created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import "ag-grid-community/dist/styles/ag-grid.css"
+import "ag-grid-community/dist/styles/ag-theme-balham.css"
+import {AgGridReact} from 'ag-grid-react'
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      columnDefs:[
+        {headerName :'سازنده',field:'make',sortable:true,filter:true},
+        {headerName :'مدل',field:'model',sortable:true,filter:true},
+        {headerName :'قیمت',field:'price',sortable:true,filter:true},
+    ],
+    rowData:[
+      {make:'Toyota1',model:'model1',price:3000},
+      {make:'Toyota2',model:'model2',price:4000},
+      {make:'Toyota3',model:'model3',price:5000},
+    ]
+    }
+  }
+  render() {
+    return (
+      <div className="ag-theme-balham" style={{direction:"rtl", width:700,height:600}}>
+        <AgGridReact columnDefs={this.state.columnDefs} rowData={this.state.rowData} />
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+    )
+  }
 }
-
-export default App;
